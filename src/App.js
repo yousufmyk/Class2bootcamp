52d56cc214b895ef224ebb68f9a410f3de83ef45
yousufmyk <yk445261@gmail.com>
2020-12-22 00:59:38 +0500

    class2code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      hello world from Muhammad Yousuf Khan
+    </div>
+  );
+}
+
+export default App;
